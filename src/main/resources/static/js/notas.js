ac3e656cb13470fd0b661d75d299f8255d1de9c3
Lucas Teixeira <lucas.teixeira34@gmail.com>
2020-07-14 00:00:52 -0300

    Função JS e CSS para exibição de notas em cores

---
+++ src/main/resources/static/js/notas.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#myTable tbody tr td").each(function () {
+        if ($(this).text() > 5)
+            $(this).addClass('verde');
+        else if ($(this).text() > 0.1 && $(this).text() <= 5)
+            $(this).addClass('vermelho');
+    });
+});
