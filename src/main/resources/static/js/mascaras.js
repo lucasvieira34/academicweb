b289cc4cda2914d285e13e44e73c313224f5cf63
Lucas Teixeira <lucas.teixeira34@gmail.com>
2020-08-12 00:30:56 -0300

    Máscaras de R$ no dashboard

--- src/main/resources/static/js/mascaras.js
+++ src/main/resources/static/js/mascaras.js
@@ -1,4 +1,34 @@
-$(document).ready(function () { 
-    var $cpf = $("#cpf");
-    $cpf.mask('000.000.000-00', {reverse: true});
+$(document).ready(function() {
+	var $cpf = $("#cpf");
+	$cpf.mask('000.000.000-00', {
+		reverse : true
+	});
+});
+
+$(document).ready(function() {
+	var $money1 = $("#money1");
+	$money1.mask('999.990,00', {
+		reverse : true
+	})
+});
+
+$(document).ready(function() {
+	var $money2 = $("#money2");
+	$money2.mask('999.990,00', {
+		reverse : true
+	})
+});
+
+$(document).ready(function() {
+	var $money3 = $("#money3");
+	$money3.mask('999.990,00', {
+		reverse : true
+	})
+});
+
+$(document).ready(function() {
+	var $money4 = $("#money4");
+	$money4.mask('999.990,00', {
+		reverse : true
+	})
 });
