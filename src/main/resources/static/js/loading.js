a6e5068c7580f22ebbb2bb2e713bb1564cf58e2f
Lucas Teixeira <lucas.teixeira34@gmail.com>
2020-09-29 18:14:06 -0300

    Spinner de processamento no cadastro de Aluno

---
+++ src/main/resources/static/js/loading.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".personalizado").on("click", function () {
+    
+    	$('html, body').animate({scrollTop:0}, 'slow');
+
+        var customElement = $("<div>", {
+            "css"   : {
+                "font-size"     : "40px",
+                "text-align"    : "center",
+                "margin-top"    : "5px",
+            },
+            "text"  : "Processando..."
+        });
+
+        $(".container-fluid").LoadingOverlay("show", {
+            imageColor: "#FA8021",
+            custom      : customElement
+        });
+        setTimeout(function () {
+            $(".container-fluid").LoadingOverlay("hide");
+        }, 8000);
+
+    });
+});
